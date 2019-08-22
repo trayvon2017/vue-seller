f292089eff4cb6f730a46cc18ae9275a04c877de
fbfatboy <690624138@qq.com>
2019-08-22 18:55:26 +0800

    header开头
    header开头

--- src/components/header/v-header.vue
+++ src/components/header/v-header.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <div class="content-wrapper">
-      <div class="avatat-box">
-        <img :src="seller.avatar" alt="">
+      <div class="avatar-box">
+        <img width="64" height="64" :src="seller.avatar" alt="">
       </div>
       <div class="info-box">
         <div class="title">
-          <span class="icon"></span>
-          <span class="seller-nam">{{seller.name}}</span>
+          <span class="icon-brand"></span>
+          <span class="seller-name">{{seller.name}}</span>
         </div>
         <div class="desc">
-          <span>{{seller.description}}</span>/<span>{{seller.deliverytime}}分送达</span>
+          <span>{{seller.description}}</span>/<span>{{seller.deliveryTime}}分送达</span>
         </div>
       </div>
       <div class="special-count">
@@ -29,7 +29,8 @@ export default {
   },
   props: {
     seller: {
-      type: Object
+      type: Object,
+      required: true
     }
   },
   methods: {
@@ -41,6 +42,30 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+.content-wrapper
+  padding: 24px 12px 18px 24px
+  color: #ffffff
+  background-color #000
+  font-size: 0px
+  .avatar-box
+    display: inline-block
+    vertical-align: top
+    img
+      border-radius: 2px
+  .info-box
+    display: inline-block
+    .title
+      .icon-brand
+        display: inline-block
+        vertical-align: top
+        width: 30px
+        height: 18px
+      .seller-name{
+        font-size: 16px
+        font-weight: bold
+        line-height: 18px
+      }
+    .desc
+      font-size: 12px
 </style>
