954061417164a1639babc6e25702954df7573e2a
trayvon2017 <690624138@qq.com>
2020-03-18 09:44:50 +0800

    解决scss找不到路径的问题

--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import './common/scss/index.scss'
 
 Vue.config.productionTip = false
-import './common/scss/index.scss'
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
