42ae07b7d9094262a28ee8d7c2d2c6e32fcba1f4
trayvon2017 <690624138@qq.com>
2020-03-20 06:58:20 +0800

    配置本地开发devserver,import别名配置

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <v-header></v-header>
     <router-view/>
   </div>
 </template>
 
 <script>
+import VHeader from 'components/v-header/v-header'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    VHeader
+  }
 }
 </script>
 
---
+++ src/components/v-header/v-header.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="header">
+    header
+</div>
+</template>
+<script>
+export default {
+  name: "VHeader",
+  data () {
+    return {
+    };
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
