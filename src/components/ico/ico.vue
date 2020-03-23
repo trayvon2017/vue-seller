87cfbe12ac77c755a7de9ec715b6bf23b33b77ca
trayvon2017 <690624138@qq.com>
2020-03-24 00:58:17 +0800

    ico组件,header基本结构完成

---
+++ src/components/ico/ico.vue
@@ -0,0 +1,106 @@
+<template>
+  <span class="ico" :class="iconClass"></span>
+</template>
+
+<script>
+export default {
+  name: 'Ico',
+  props: {
+    size: {
+      type: String,
+      default() {
+        return 'small'
+      }
+    },
+    type: {
+      type: String,
+      default() {
+        return 'decrease_1'
+      }
+    }
+  },
+  computed: {
+    iconClass() {
+      return `ico-${this.type} ico-${this.size}`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~common/scss/mixin.scss';
+.ico {
+  display: inline-block;
+  vertical-align: middle;
+  &.ico-small {
+    width: 12px;
+    height: 12px;
+    background-size: 12px 12px;
+    &.ico-decrease_1 {
+      @include bg-image('decrease_1');
+    }
+    &.ico-decrease_2 {
+      @include bg-image('decrease_3');
+    }
+    &.ico-discount_1 {
+      @include bg-image('discount_1');
+    }
+    &.ico-discount_2 {
+      @include bg-image('discount_3');
+    }
+    &.ico-guarantee_1 {
+      @include bg-image('guarantee_1');
+    }
+    &.ico-guarantee_2 {
+      @include bg-image('guarantee_3');
+    }
+    &.ico-invoice_1 {
+      @include bg-image('invoice_1');
+    }
+    &.ico-invoice_2 {
+      @include bg-image('invoice_3');
+    }
+    &.ico-special_1 {
+      @include bg-image('special_1');
+    }
+    &.ico-special_2 {
+      @include bg-image('special_3');
+    }
+  }
+  &.ico-large {
+    width: 16px;
+    height: 16px;
+    background-size: 16px 16px;
+    &.ico-decrease_1 {
+      @include bg-image('decrease_2');
+    }
+    &.ico-decrease_2 {
+      @include bg-image('decrease_4');
+    }
+    &.ico-discount_1 {
+      @include bg-image('discount_2');
+    }
+    &.ico-discount_2 {
+      @include bg-image('discount_4');
+    }
+    &.ico-guarantee_1 {
+      @include bg-image('guarantee_2');
+    }
+    &.ico-guarantee_1 {
+      @include bg-image('guarantee_4');
+    }
+    &.ico-invoice_1 {
+      @include bg-image('invoice_2');
+    }
+    &.ico-invoice_2 {
+      @include bg-image('invoice_4');
+    }
+    &.ico-special_1 {
+      @include bg-image('special_2');
+    }
+    &.ico-special_2 {
+      @include bg-image('special_4');
+    }
+  }
+}
+</style>
--- src/components/v-header/v-header.vue
+++ src/components/v-header/v-header.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="header">
+    <div class="bg-wrapper">
+      <img :src="seller.avatar" alt="" />
+    </div>
     <div class="content-wrapper">
       <div class="avatar">
-        <img :src="seller.avatar" alt="" />
+        <img :src="seller.avatar" alt />
       </div>
       <div class="content">
         <div class="title">
@@ -13,16 +16,24 @@
           {{ seller.description }}/{{ seller.deliveryTime }}分送达
         </div>
         <div v-if="seller.supports" class="supports">
-          <span class="support-ico"></span>
+          <ico type="decrease_1"></ico>
           <span class="support-desc">{{ seller.supports[0].description }}</span>
         </div>
+      </div>
+      <div class="sales">
+        <span class="count">5个</span>
+        <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
   </div>
 </template>
 <script>
+import Ico from 'components/ico/ico'
 export default {
   name: 'VHeader',
+  components: {
+    Ico
+  },
   props: {
     seller: {
       type: Object,
@@ -38,10 +49,14 @@ export default {
 </script>
 <style lang="scss" scoped>
 @import '~common/scss/const.scss';
+@import '~common/scss/mixin.scss';
 .header {
+  position: relative;
+  background-color: rgba(7, 17, 27, 0.5);
   .content-wrapper {
+    position: relative;
     padding: 24px 0 18px 24px;
-    background: black;
+    color: white;
     font-size: 0;
     .avatar {
       display: inline-block;
@@ -54,20 +69,69 @@ export default {
     // TODO: 头部样式
     .content {
       display: inline-block;
+      vertical-align: top;
       margin-left: 16px;
-      height: 100%;
-      color: $color-white;
+      // height: 100%;
       .title {
+        margin-top: 2px;
         font-size: 16px;
         font-weight: bold;
+        & > span {
+          vertical-align: middle;
+        }
+        .brand {
+          display: inline-block;
+          width: 30px;
+          height: 18px;
+          background-size: 30px 18px;
+          background-repeat: no-repeat;
+          background-position: center;
+          @include bg-image('./brand');
+        }
       }
       .desc {
+        margin: 8px 0 10px 0;
         font-size: 12px;
       }
       .supports {
+        margin-bottom: 2px;
+        font-size: 10px;
+        .support-desc {
+          vertical-align: middle;
+        }
+      }
+    }
+    .sales {
+      position: absolute;
+      right: 12px;
+      bottom: 14px;
+      padding: 0 8px;
+      height: 24px;
+      line-height: 24px;
+      border-radius: 14px;
+      background: rgba(0, 0, 0, 0.2);
+      text-align: center;
+      .count {
+        vertical-align: top;
+        font-size: 10px;
+      }
+      .icon-keyboard_arrow_right {
+        margin-left: 2px;
+        line-height: 24px;
         font-size: 10px;
       }
     }
+  }
+  .bg-wrapper {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    overflow: hidden;
+    z-index: -1;
+    font-size: 0;
+    filter: blur(10px);
   }
 }
 </style>
